5a75f7a44d701c083c21ae1eb8008f1c05fe8a0a
CheChireCat0430 <vickey043011@gmail.com>
2021-01-20 02:28:54 +0900

    3강 실습 추가

---
+++ book1/chapter1/1.10.py
@@ -0,0 +1,18 @@
+''' 
+클래스 기본
+'''
+
+class Man :
+    def __init__(self, name) :
+        self.name = name
+        print("Initialized")
+
+    def hello(self) :
+        print("hello " + self.name + "!")
+
+    def goodbye(self) :
+        print("Good-bye " + self.name + "!")
+
+m = Man("David")
+m.hello()
+m.goodbye()
